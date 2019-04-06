788e3037721ca6ced0d704f8c14cbb02fc89f991
isavnin <vladkalyuzhnyu@gmail.com>
2019-04-06 10:25:18 +0300

    changes

--- browser/src/main/java/androidovshchik/browser/NativeWebView.kt
+++ browser/src/main/java/androidovshchik/browser/NativeWebView.kt
@@ -8,14 +8,16 @@ import android.os.Build
 import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
+import androidovshchik.browser.extensions.await
 import kotlinx.coroutines.*
-import okhttp3.*
+import okhttp3.Call
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import org.jsoup.Jsoup
 import org.jsoup.parser.Tag
-import timber.log.Timber
 import java.io.IOException
 
-class NativeWebView : FrameLayout, Callback, CoroutineScope {
+class NativeWebView : FrameLayout, CoroutineScope {
 
     private val job = SupervisorJob()
 
@@ -23,13 +25,9 @@ class NativeWebView : FrameLayout, Callback, CoroutineScope {
         dispatcher().maxRequests = 4
     }
 
-    private var webViewClient: BrowserClient? = null
-
     private var currentUrl: String? = null
 
-    private var documentStyle = ""
-
-    private var documentScript = ""
+    var webViewClient: BrowserClient? = null
 
     constructor(context: Context) : this(context, null)
 
@@ -46,118 +44,106 @@ class NativeWebView : FrameLayout, Callback, CoroutineScope {
     )
 
     fun setWebViewClient(init: BrowserClient.() -> Unit) {
-        webViewClient = BrowserClient().apply {
-            init()
-        }
+        webViewClient = BrowserClient().apply { init() }
     }
 
     fun loadUrl(url: String) {
         stopLoading()
         currentUrl = url
-        makeRequest(url, url)
-    }
-
-    private fun makeRequest(url: String, tag: Any?) {
-        val formattedUrl = when {
-            url.trim().startsWith("//") -> "http://$url"
-            else -> url.trim()
-        }
-        val request = Request.Builder()
-            .url(formattedUrl)
-            .tag(tag)
-            .build()
-        httpClient.newCall(request)
-            .enqueue(this)
-    }
-
-    override fun onResponse(call: Call, response: Response) {
-        val url = call.request()
-            .url()
-            .toString()
-        if (!response.isSuccessful) {
+        //if (!response.isSuccessful) {
             //webViewClient?.onReceivedError(url, response.code(), response.message())
-            return
-        }
-        val tag = call.request()
-            .tag()
-        val body = response.body()
+        // }
+        var styles = ""
+        var scripts = ""
         launch {
+            val body = makeRequest(url, url).await(-1).body()
+            val bodyType = "${body?.contentType()?.type()}/${body?.contentType()?.subtype()}".toLowerCase()
+            when (bodyType) {
+                "text/html" -> {
+                    val html = ElementViewGroup(context.applicationContext).apply {
+                        id = R.id.page_content
+                        layoutParams = FrameLayout.LayoutParams(
+                            FrameLayout.LayoutParams.MATCH_PARENT,
+                            FrameLayout.LayoutParams.MATCH_PARENT
+                        )
+                        visibility = View.INVISIBLE
+                        tag = Tag.valueOf("html")
+                    }
+                    addView(html)
+                }
+                else -> {
+                    return@launch
+                }
+            }
             withContext(Dispatchers.IO) {
-                when (body?.contentType()?.type()) {
-                    "text" -> {
-                        when (body.contentType()?.subtype()) {
-                            "html" -> {
-                                if (childCount >= 1) {
-                                    return@withContext
-                                }
-                                val html = ElementViewGroup(context.applicationContext).apply {
-                                    id = R.id.page_content
-                                    layoutParams = FrameLayout.LayoutParams(
-                                        FrameLayout.LayoutParams.MATCH_PARENT,
-                                        FrameLayout.LayoutParams.MATCH_PARENT
-                                    )
-                                    visibility = View.INVISIBLE
-                                    setTag(Tag.valueOf("html"))
-                                }
-                                addView(html)
-                                Jsoup.parse(body.string() ?: "").apply {
-                                    //normalise()
-                                    select("link")
-                                        .forEach {
-                                            if (it.attributes().hasKeyIgnoreCase("href")) {
-                                                loadResource(it.attributes().getIgnoreCase("href"), tag)
-                                            }
-                                        }
-                                    select("style")
-                                        .forEach {
-                                            documentStyle += it.data()
-                                        }
-                                    select("script")
-                                        .forEach {
-                                            if (it.attributes().hasKeyIgnoreCase("src")) {
-                                                loadResource(it.attributes().getIgnoreCase("src"), tag)
-                                            } else {
-                                                documentScript += it.data()
-                                            }
-                                        }
-                                }
-                                Timber.d(documentStyle)
-                                Timber.d(documentScript)
-                            }
-                            "css" -> {
-                                Timber.d(body.string())
-                            }
+                val job = Job(job)
+                Jsoup.parse(body?.string() ?: "").apply {
+                    //normalise()
+                    select("style")
+                        .forEach {
+                            styles += it.data()
                         }
-                    }
-                    "application" -> {
-                        when (body.contentType()?.subtype()) {
-                            "javascript", "x-javascript" -> {
-                                Timber.d(body.string())
-                            }
-                            "octet-stream" -> {
+                    var i = 0
+                    select("link") + select("script")
+                        .forEach {
+                            when {
+                                it.attributes().hasKeyIgnoreCase("href") or it.attributes().hasKeyIgnoreCase("src") -> {
+                                    val resourceUrl = when {
+                                        it.attributes().hasKeyIgnoreCase("href") -> it.attributes().getIgnoreCase("href")
+                                        else -> it.attributes().getIgnoreCase("src")
+                                    }
+                                    val call = loadResource(resourceUrl, url) ?: return@forEach
+                                    i++
+                                    launch(job) {
+                                        val resource = call.await(i).body()
+                                        val resourceType =
+                                            "${resource?.contentType()?.type()}/${resource?.contentType()?.subtype()}".toLowerCase()
+                                        when (resourceType) {
+                                            "text/css" -> {
 
+                                            }
+                                            "application/javascript", "application/x-javascript" -> {
+
+                                            }
+                                            "application/octet-stream" -> {
+
+                                            }
+                                        }
+                                    }
+                                }
+                                else -> if (it.tagName() == "script") {
+                                    scripts += it.data()
+                                }
                             }
                         }
-                    }
+                }
+                job.children.forEach {
+                    it.join()
                 }
             }
         }
     }
 
-    private fun loadResource(url: String, tag: Any?) {
-        val formattedUrl = url.split("?")[0]
+    @Throws(IOException::class)
+    private fun loadResource(url: String, tag: Any?): Call? {
+        var formattedUrl = url.split("?")[0]
             .trim()
-        if (".(css|js|eot|otf|ttf|woff|woff2)$".toRegex().matches(formattedUrl)) {
-            makeRequest(formattedUrl, tag)
+        if (formattedUrl.startsWith("//")) {
+            formattedUrl = "http:$formattedUrl"
         }
+        if (".(css|js|eot|otf|ttf|woff|woff2)$".toRegex().matches(formattedUrl)) {
+            return makeRequest(formattedUrl, tag)
+        }
+        return null
     }
 
-    override fun onFailure(call: Call, e: IOException) {
-        /*if (call.isExecuted) {
-            webViewClient?.onReceivedError(call.request()
-                .url()
-                .toString(), call., e.)
-        }*/
+    @Throws(IOException::class)
+    private fun makeRequest(url: String, tag: Any?): Call {
+        val request = Request.Builder()
+            .url(url)
+            .tag(tag)
+            .build()
+        return httpClient.newCall(request)
     }
 
     fun reload() {
@@ -166,29 +152,11 @@ class NativeWebView : FrameLayout, Callback, CoroutineScope {
     }
 
     fun stopLoading() {
-        coroutineContext.cancelChildren()
-        val dispatcher = httpClient.dispatcher()
-        for (call in dispatcher.runningCalls()) {
-            if (call.request().tag()?.equals(currentUrl) == true) {
-                call.cancel()
-            }
-        }
-        for (call in dispatcher.queuedCalls()) {
-            if (call.request().tag()?.equals(currentUrl) == true) {
-                call.cancel()
-            }
-        }
+        job.cancelChildren()
     }
 
     fun destroy() {
-        coroutineContext.cancelChildren()
-        val dispatcher = httpClient.dispatcher()
-        for (call in dispatcher.runningCalls()) {
-            call.cancel()
-        }
-        for (call in dispatcher.queuedCalls()) {
-            call.cancel()
-        }
+        stopLoading()
     }
 
     override val coroutineContext = Dispatchers.Main + job
